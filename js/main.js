cf457ae7db8141baf7203de2226389ca49709ee5
Luqman Hassan <luqman1isb@gmail.com>
2024-08-09 16:46:55 +0500

    did add FAQ sectin

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// FAQ accordion
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faqContainer = document.querySelector('.faq-content');
+
+    faqContainer.addEventListener('click', (e) =>{
+        const groupHeader = e.target.closest('.faq-group-header');
+
+        if(!groupHeader) return;
+
+        const group =groupHeader.parentElement;
+        const groupBody = group.querySelector('.faq-group-body');
+        const icon = groupHeader.querySelector('i')
+
+        //toggle icon
+
+        icon.classList.toggle('fa-plus')
+        icon.classList.toggle('fa-minus')
+
+        //toggle visibility of body
+
+        
+        groupBody.classList.toggle('open');
+
+        // close other open FAQ bodies
+
+        const otherGroup = faqContainer.querySelectorAll('.faq-group');
+
+        otherGroup.forEach(() =>{
+            if(otherGroup !== group){
+                const otherGroupBody = otherGroup.querySelector('.faq-group-body');
+                const otherIcon = otherGroup.querySelector('.faq-group-header i');
+
+
+                otherGroupBody.classList.remove('open');
+                otherIcon.classList.remove('fa-minus');
+                otherIcon.classList.add('fa-plus');
+            }
+
+
+        });
+    });
+});
